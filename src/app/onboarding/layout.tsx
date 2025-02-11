3073d0d9f4a28553a4f2c51b41219c370a72b4e1
Cristian <cristian10261@gmail.com>
2025-02-11 09:23:58 -0500

    merge main

---
+++ src/app/onboarding/layout.tsx
@@ -0,0 +1,9 @@
+export default function OnboardingLayout({ children }: { children: React.ReactNode }) {
+    return (
+      <div>
+        <h1>Onboarding Layout</h1>
+        {children}
+      </div>
+    );
+  }
+  
