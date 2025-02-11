e8039f37b75ef54c2d1eedbad37038fa72b4d1e6
Cristian <cristian10261@gmail.com>
2025-02-11 13:04:01 -0500

    fix color

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { LazyImageRenderer } from "lazy-image-renderer";
 import styles from "./Home.module.scss";
 
 export default function Home() {
@@ -9,6 +10,12 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <main className={styles.main}>
+        <LazyImageRenderer
+          src="icon/Logo.svg"
+          alt="KloudID Logo"
+          width={200}
+          height={100}
+        />
         <h1 className={styles.title}>Welcome to KloudID</h1>
         <p className={styles.description}>
           We are excited to have you on board. Below you can start the process
